1ee611c5dd2d7d866fe2ce120da7eb4e3206e81f
RamizSami <ramizsami95@gmail.com>
2018-09-23 22:38:22 +0500

    Loaded the theme in django project, initiated ArcGIS scene map in the theme, added login window

--- dryver_django/dryver_django/settings.py
+++ dryver_django/dryver_django/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'dryver_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
             ],
         },
     },
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
