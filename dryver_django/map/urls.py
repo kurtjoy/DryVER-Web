8470cda72d0b608d4dd0702ca42abe312e579215
RamizSami <ramizsami95@gmail.com>
2019-01-31 23:24:02 +0500

    Added Windy widget

--- dryver_django/map/urls.py
+++ dryver_django/map/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('windy', views.Windy.as_view(), name="Windy"),
 ]
--- dryver_django/map/views.py
+++ dryver_django/map/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import request
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
+
+
+class Windy(TemplateView):
+    template_name = 'windy.html'
 
 @login_required
 def index(request):
