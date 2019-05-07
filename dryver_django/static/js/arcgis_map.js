d18820bb75189746660ec85240931f13791a4836
RamizSami <ramizsami95@gmail.com>
2019-05-07 17:03:21 +0500

    Added 2D measurement tool

--- dryver_django/static/js/arcgis_map.js
+++ dryver_django/static/js/arcgis_map.js
@@ -13,7 +13,8 @@ $(document).ready(function () {
         "esri/widgets/LayerList",
         "esri/widgets/Compass",
         "esri/widgets/ScaleBar",
-        "dojo/domReady!"
+        "esri/widgets/DistanceMeasurement2D",
+        "dojo/domReady!",
         ], function(
             Map,
             SceneView,
@@ -25,7 +26,8 @@ $(document).ready(function () {
             Expand,
             LayerList,
             Compass,
-            ScaleBar,) {
+            ScaleBar,
+            DistanceMeasurement2D,) {
         var map = new Map({
             basemap: "satellite",
             // ground: "world-elevation"
@@ -36,169 +38,6 @@ $(document).ready(function () {
             center: [162, -77.5],
             zoom: 7,
         });
-
-        // renderers and symbologies
-        var green = {
-            type: "simple", // autocasts as new SimpleRenderer()
-            symbol: {
-                  type: "point-3d", // autocasts as new PointSymbol3D()
-                  symbolLayers: [{
-                    type: "icon",
-                    anchor: "center",
-                    outline: {
-                        size: 1
-                    },
-                    material: {
-                        color: [0, 255, 0, 0.5]
-                    }
-                }]
-            }
-        };
-
-        var yellow = {
-            type: "simple", // autocasts as new SimpleRenderer()
-            symbol: {
-                  type: "point-3d", // autocasts as new PointSymbol3D()
-                  symbolLayers: [{
-                    type: "icon",
-                    anchor: "center",
-                    outline: {
-                        size: 1
-                    },
-                    material: {
-                        color: [178, 160, 40, 0.5]
-                    }
-                }]
-            }
-        };
-
-        var blue = {
-            type: "simple", // autocasts as new SimpleRenderer()
-            symbol: {
-                  type: "point-3d", // autocasts as new PointSymbol3D()
-                  symbolLayers: [{
-                    type: "icon",
-                    anchor: "center",
-                    outline: {
-                        size: 1
-                    },
-                    material: {
-                        color: [0, 0, 255, 0.5]
-                    }
-                }]
-            }
-        };
-
-        // Popup templates
-        var aspaTemp = {title: "{NAME}",
-            content: [{
-                type: "media",
-                mediaInfos: [{type: "image", value: {sourceURL: "{IMAGE}"}}]},
-                        {type: "text",
-                        text: "{DESC}<br><br>" + "Helicopter Pad: {HELO}<br><br>" + "PDF Map:"}	 
-            ]
-        };
-
-        var nztabsTemp = {title: "Site: {VALLEY} {FIELD_ID}<br>" + "Predicted Fungal Biodiversity: {FUNGI_S}<br>" + "Predicted Cyanobacterial Biodiversity: {CYANO_S}<br>" + "Predicted Total Biodiversity: {TAXA_S}", content: [{
-            type: "fields",
-            fieldInfos: [{
-                fieldName: "ELV_MASL",
-                visible: true,
-                label: "Elevation (masl)"
-            },{
-                fieldName: "SLOPE",
-                visible: true,
-                label: "Slope (degrees)"
-            },{
-                fieldName: "ASPECT",
-                visible: true,
-                label: "Aspect (degrees)"
-            },{
-                fieldName: "COAST_M",
-                visible: true,
-                label: "Distance to Coast (m)"
-             },{
-                fieldName: "SOIL_WATER",
-                visible: true,
-                label: "Soil Water)"
-            },{
-                fieldName: "PH",
-                visible: true,
-                label: "Soil pH"
-            },{
-                fieldName: "S_MEAN_TEMP",
-                visible: true,
-                label: "Summer Mean Soil Temperature (degrees)"
-            },{
-                fieldName: "WETNESS_AL",
-                visible: true,
-                label: "Wetness Index (all)"
-            },{
-                fieldName: "WETNESS_DJ",
-                visible: true,
-                label: "Wetness Index (Dec-Jan)"}
-            ]}
-        ]};
-
-        var	geochemTemp = {title:"Site ID:{FIELD_ID}", content: [{
-            type: "text",
-            text: "<br>Analysed at the University of Waikato on {ICPMS_DATE}. All measured concentrations in Parts Per Billion"},
-                {type: "fields",fieldInfos:[ 
-                    {fieldName: "B_10", label: "Boron-10:"},
-                    {fieldName: "Na_23",label: "Sodium-23:"},
-                    {fieldName: "Mg_24",label: "Magnesium-24:"},
-                    {fieldName: "Al_27",label: "Aluminium-27:"},
-                    {fieldName: "P_31",label: "Phosphorus-31:"},
-                    {fieldName: "S_34",label: "Sulfur-34:"},
-                    {fieldName: "K_39",label: "Potassium-39:"},
-                    {fieldName: "Ca_43",label: "Calcium-43:"},
-                    {fieldName: "V_51",label: "Vanadium-51:"},
-                    {fieldName: "Cr_52",label: "Chromium-52:"},
-                    {fieldName: "Fe_54",label: "Iron-54:"},
-                    {fieldName: "Co_59",label: "Cobalt-59:"},
-                    {fieldName: "Ni_60",label: "Nickle-60:"},
-                    {fieldName: "Cu_63",label: "Copper-63:"},
-                    {fieldName: "Zn_66",label: "Zinc-66:"},
-                    {fieldName: "As_75",label: "Arsenic-75:"},
-                    {fieldName: "Se_82",label: "Selenium-82:"},
-                    {fieldName: "Sr_88",label: "Strontium-88:"},
-                    {fieldName: "Ag_109",label: "Silver-109:"},
-                    {fieldName: "Cd_111",label: "Cadmium-111:"},
-                    {fieldName: "Ba_137",label: "Barium-137:"},
-                    {fieldName: "Pb_207",label: "Lead-207:"},
-                    {fieldName: "U_238",label: "Uranium-238:"} 
-                ]}
-            ]
-        };
-
-        var geolTemp = {title: "{MAP_UNIT}" + " ({DRYVER_GROUP})", content: [{
-            type: "fields",
-                    fieldInfos: [{
-                        fieldName: "TERRANE",
-                        visible: true,
-                        label: "Terrane Type:"
-                    },{
-                        fieldName: "STRAT_UNIT",
-                        visible: true,
-                        label: "Stratigraphic Name:"
-                    },{
-                        fieldName: "ROCK_GROUP",
-                        visible: true,
-                        label: "Rock Group:"
-                    },{
-                        fieldName: "AGE_INDEX",
-                        visible: true,
-                        label: "Relative Age:"
-                    },{
-                        fieldName: "DESCRIPTIO",
-                        visible: true,
-                        label: "Description:"}
-                    ]
-                }
-            ]
-        };
-
-        var agarTemp = {title: "{SID}"};
                             
         // layers
 
@@ -256,7 +95,6 @@ $(document).ready(function () {
             ]
         });
 
-        // continue here
         asmaLayer = new MapImageLayer({
             url: "https://trugis.sci.waikato.ac.nz/arcgis/rest/services/DRYVER/ASMA/MapServer",
             title: "ASMA",
@@ -337,8 +175,8 @@ $(document).ready(function () {
                 {
                   id: 0,
                   title: "nzTABS Sample Sites",
-                  visible: true,
-                //   renderer: rendererToUse
+                  visible: false,
+                //   renderer: nztabsTemp
                 },
             ]
         });
@@ -426,14 +264,11 @@ $(document).ready(function () {
             view: view
         });
 
-        // adds the compass to the top left corner of the MapView
-        view.ui.add(compass, "top-right");
-
         var scaleBar = new ScaleBar({
             view: view
         });
 
-        view.ui.add(scaleBar, "bottom-right");
+        
         
 
         // legend
@@ -461,10 +296,7 @@ $(document).ready(function () {
             expandIconClass: "esri-icon-collection"
         });
         
-        view.ui.add(legendEx,{
-            position: "top-right"
         
-        });
 
         var layerList = new LayerList({
             container: document.createElement("div"),
@@ -476,10 +308,27 @@ $(document).ready(function () {
             // expandTooltip: "Expand LayerList", // optional, defaults to "Expand" for English locale
             view: view,
             content: layerList.domNode
+        });        
+
+        // https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-DistanceMeasurement2D.html
+        // https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-AreaMeasurement2D.html
+        var distanceMeasurementWidget = new DistanceMeasurement2D({
+            view: view,
+            label: "Measure Distance",
+        });
+
+        view.ui.add(distanceMeasurementWidget, "top-right");
+
+        view.ui.add(compass, "top-right");
+
+        view.ui.add(scaleBar, "bottom-right");
+
+        view.ui.add(legendEx,{
+            position: "top-right"
         });
 
         view.ui.add(layerListExpand, "top-right");
-
+        
     });
 })
 
@@ -530,3 +379,5 @@ $('.drop-down').click(function(){
     var target = $(this).attr('data-target');
     $('.'+target).toggleClass('hide');
 });
+
+// Accessor#set Invalid property value, value needs to be one of 'esri.renderers.HeatmapRenderer', 'esri.renderers.SimpleRenderer', 'esri.renderers.UniqueValueRenderer', 'esri.renderers.ClassBreaksRenderer', or a plain object that can autocast (having .type = 'heatmap', 'simple', 'unique-value', 'class-breaks')
